3a49bca777531ddcb1dbd88dec748db8f83f44f2
AnahiFigueroa <af1009@georgetown.edu>
2020-04-06 14:44:07 -0400

    Create ChatHandler.java

---
+++ ChatHandler.java
@@ -0,0 +1,16 @@
+import java.util.Vector; 
+
+
+public class ChatHandler extends Thread {
+	
+	static final int MAXQUEUE = 5; 
+    public static Vector<String> messages = new Vector <String>(); 
+    
+	public static void main(String[] args) {
+		
+     
+     
+     
+     
+	}  
+}
