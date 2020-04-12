cbde65f9f2bd817ce0efadd2e42d1cad388a9bf1
AnahiFigueroa <af1009@georgetown.edu>
2020-04-12 13:56:44 -0400

    Update ChattyChatChatClient.java

--- ChattyChatChatClient.java
+++ ChattyChatChatClient.java
@@ -1,59 +1,59 @@
-import java.io.*; 
-import java.net.*; 
+import java.io.*;
+import java.net.*;
 
 
 public class ChattyChatChatClient {
 
-	
+
 	public static void main (String[] args) {
-		
+
 		String hostname = args[0];   //taking in a string from argument
-		Socket socket = null; 
+		Socket socket = null;
 		int portNumber = Integer.parseInt(args[1]);
-		
+
 		try {
-			
-			socket = new Socket (hostname, portNumber); 
-			BufferedReader in = 
+
+			socket = new Socket (hostname, portNumber);
+			BufferedReader in =
 				new BufferedReader(
 				new InputStreamReader(
 				socket.getInputStream()));
-			
-			PrintWriter out = new PrintWriter(socket.getOutputStream(),true); 
-			
+
+			PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+
 			//one thread needs to send messages and another threads needs to read messages
 			//each thread needs to call @override public void run
-			
+
 			Thread sendmessage = new Thread(new Runnable() {
-				@Override
+				  @Override
 				  public void run() {
-					BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in)); 
+					BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
 					boolean done = false; 
 					while (!done) {
-						String userInput = ""; 
+						String userInput = "";
 						try {
 							userInput = userIn.readLine();
 						} catch (IOException e) {
 							e.printStackTrace();
 						}
 						if (userInput.contentEquals("/quit")) {
-							done = true; 
+							done = true;
 						}
-						out.println(userInput); 
+						out.println(userInput);
 						out.flush();                                  //resets it
-						
+
 					}//END while
 				}
-				
-			}); 
-		
+
+			});
+
 
 			Thread readmessage = new Thread(new Runnable() {
 				@Override
 				  public void run() {
-					boolean done = false; 
+					boolean done = false;
 					while (!done) {
-						String userInput = ""; 
+						String userInput = "";
 						try {
 							userInput = in.readLine();
 						} catch (IOException e) {
@@ -61,28 +61,22 @@ public class ChattyChatChatClient {
 							e.printStackTrace();
 						}
 						if (userInput.contentEquals("/quit")) {
-							done = true; 
+							done = true;
 						}
-						System.out.println(userInput); 
-						
+						System.out.println(userInput);
+
 					}//END while
 				}
-				
-			}); 
 
-			
+			});
+
 	sendmessage.start();
 	readmessage.start();
-	
+
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}//END main()
-		  
+
   } //END client
-  
-  	
-  			
-  			
-	
