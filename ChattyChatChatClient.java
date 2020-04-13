e5e02df957b9e2b565ff66ab8b60843be5bb9dde
AnahiFigueroa <af1009@georgetown.edu>
2020-04-13 15:20:33 -0400

    Update ChattyChatChatClient.java

--- ChattyChatChatClient.java
+++ ChattyChatChatClient.java
@@ -14,66 +14,66 @@ public class ChattyChatChatClient {
 		try {
 
 			socket = new Socket (hostname, portNumber);
-			new sendmessage(socket).start(); 
-			new readmessage(socket).start(); 
-//			BufferedReader in =
-//				new BufferedReader(
-//				new InputStreamReader(
-//				socket.getInputStream()));
+			BufferedReader in =
+				new BufferedReader(
+				new InputStreamReader(
+				socket.getInputStream()));
 
-//			PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+			// PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
 
 			//one thread needs to send messages and another threads needs to read messages
 			//each thread needs to call @override public void run
-//
-//			Thread sendmessage = new Thread(new Runnable() {
-//				  @Override
-//				  public void run() {
-//					BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
-//					boolean done = false; 
-//					while (!done) {
-//						String userInput = "";
-//						try {
-//							userInput = userIn.readLine();
-//						} catch (IOException e) {
-//							e.printStackTrace();
-//						}
-////						if (userInput.contentEquals("/quit")) {
-////							done = true;
-////						}
-//						out.println(userInput);
-//						out.flush();                                  //resets it
-//
-//					}//END while
-//				}
-//
-//			});
+
+			Thread sendmessage = new Thread(new Runnable() {
+				  @Override
+				  public void run() {
+					// BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
+					boolean done = false;
+					while (!done) {
+						PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+						BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
+						String userInput = "";
+						try {
+							userInput = userIn.readLine();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						// if (userInput.contentEquals("/quit")) {
+						// 	done = true;
+						// }
+						out.println(userInput);
+						out.flush();                                  //resets it
+
+					}//END while
+				}
+
+			});
 
 
-//			Thread readmessage = new Thread(new Runnable() {
-//				@Override
-//				  public void run() {
-//					boolean done = false;
-//					while (!done) {
-//						String userInput = "";
-//						try {
-//							userInput = in.readLine();
-//						} catch (IOException e) {
-//							// TODO Auto-generated catch block
-//							e.printStackTrace();
-//						}
-////						if (userInput.contentEquals("/quit")) {
-////							done = true;
-////						}
-//						System.out.println(userInput);
-//
-//					}//END while
-//				}
-//
-//			});
+			Thread readmessage = new Thread(new Runnable() {
+				@Override
+				  public void run() {
+					boolean done = false;
+					while (!done) {
+						String userInput = "";
+						try {
+							userInput = in.readLine();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						// if (userInput.contentEquals("/quit")) {
+						// 	done = true;
+						// }
+						System.out.println(userInput);
 
-//	sendmessage.start();
-//	readmessage.start();
+					}//END while
+				}
+
+			});
+
+	sendmessage.start();
+	readmessage.start();
 
 		}catch (Exception e) {
 			e.printStackTrace();
@@ -82,4 +82,3 @@ public class ChattyChatChatClient {
 	}//END main()
 
   } //END client
-
