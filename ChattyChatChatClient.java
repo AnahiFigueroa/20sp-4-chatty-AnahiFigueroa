d53453e759eb003a2c5660e97f59bd17f2b95446
AnahiFigueroa <af1009@georgetown.edu>
2020-04-15 14:07:06 -0400

    Update ChattyChatChatClient.java

--- ChattyChatChatClient.java
+++ ChattyChatChatClient.java
@@ -1,99 +1,95 @@
-import java.io.*;
-import java.net.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 
-public class ChattyChatChatClient {
+public class ChatHandler implements Runnable {
+	int clientNumber = 0;
+	String clientName = "";
+	DataInputStream dis = null;
+  DataOutputStream dos = null; 
+	Socket s;
+
+	public ChatHandler(Socket socket, int i, String a ) {
+		clientNumber = i;
+		clientName = a;
+		s = socket;
+		this.dis = dis;
+		this.dos = dos;
+	}
 
 
-	public static void main (String[] args) {
-
-		String hostname = args[0];   //taking in a string from argument
-		Socket socket = null;
-		int portNumber = Integer.parseInt(args[1]);
-		Scanner scn = new Scanner(System.in);
-
-
-
+	@SuppressWarnings("null")
+	@Override
+	public void run() {
+		PrintWriter out = null;
 		try {
-
-			socket = new Socket (hostname, portNumber);
-			DataInputStream dis = new DataInputStream(socket.getInputStream());
-	        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-
-//			BufferedReader in =
-//				new BufferedReader(
-//				new InputStreamReader(
-//				socket.getInputStream()));
-
-			//PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-
-			//one thread needs to send messages and another threads needs to read messages
-			//each thread needs to call @override public void run
-
-			Thread sendmessage = new Thread(new Runnable() {
-				  @Override
-				  public void run() {
-
-				//  BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
-					boolean done = false;
-					while (!done) {
-
-						//String userInput = "";
-						String userInput = scn.nextLine();
-						try {
-
-							//userInput = userIn.readLine();
-							dos.writeUTF(userInput);
-	            System.out.println("Sending in message:");
-
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-
-						// out.println(userInput);
-					  // dos.flush();                                  //resets it
-
-					}//END while
-				}
-
-			});
-
-
-			Thread readmessage = new Thread(new Runnable() {
-				@Override
-				  public void run() {
-					boolean done = false;
-
-					while (!done) {
-						System.out.println("reading message");
-					//	String userInput = "";
-						try {
-              String userInput = dis.readUTF();
-						//	userInput = in.readLine();
-							System.out.println(userInput);
-
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						// if (userInput.contentEquals("/quit")) {
-						// 	done = true;
-						// }
-						   // System.out.println(userInput);
-
-					}//END while
-				}
-
-			});
-
-	sendmessage.start();
-	readmessage.start();
-
-		}catch (Exception e) {
-			e.printStackTrace();
+			out = new PrintWriter(s.getOutputStream(), true);
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
+		BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
+		boolean done = false;
+		while (!done) {
+			String userInput = "";
+			try {
+				userInput = userIn.readLine();
+				userInput.split("");
+				String[] input = null;
 
-	}//END main()
+				if(input[0] == "/quit") {
+					done = true;
+					ChattyChatChatServer.chathandlers.remove(this); //removing chathandler from vector
+					System.out.println("Disconnecting from server: end of program");
+					s.close();
+				}
 
-  } //END client
+				else if(input[0] == "/dm") {
+					String message = "";
+					for(int i = 2; i < input.length; i++) {
+						message += input[i] + " ";
+					}
+					for ( ChatHandler chat: ChattyChatChatServer.chathandlers) {
+						if(input[1]== chat.clientName) {
+							PrintWriter temp = new PrintWriter(chat.s.getOutputStream(), true);
+							temp.println(message); //printing to the person's chathandler
+						}
+					}
+
+
+				}
+				else if (input[0] == "/nick") {
+					clientName = input[1];
+				}
+				else {
+					String message = "";
+					for(int i = 0; i < input.length; i++) {
+						message += input[i] + " ";
+					}
+					for ( ChatHandler chat: ChattyChatChatServer.chathandlers) {
+							PrintWriter temp = new PrintWriter(chat.s.getOutputStream(), true);
+							temp.println(message); //printing to the person's chathandler
+
+					}
+
+
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (userInput.contentEquals("/quit")) {
+				done = true;
+			}
+			out.println(userInput);
+			out.flush();                                  //resets it
+
+		}//END while
+
+	}
+
+}//Chat handler class
